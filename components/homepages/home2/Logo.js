d5e210a0bd130bfe6422844a2aaf6a980aa0f917
Mohed Nazari <astro@Moheds-MacBook-Pro.local>
2023-11-27 18:39:41 +0000

    fixed trusted partners position

--- components/homepages/home2/Logo.js
+++ components/homepages/home2/Logo.js
@@ -9,18 +9,15 @@ export default function Logo() {
                     <div className="row">
                         <div className="col-md-8 m-auto">
                             <div className="heading4 text-center" data-aos="fade-up" data-aos-duration={900}>
-                                <h4>Our Trusted Partners</h4>
+                                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                                    <h4>Our Trusted Partners</h4>
+                                    <LogoSlider1 />
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12">
-                            <LogoSlider1 />
                         </div>
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
