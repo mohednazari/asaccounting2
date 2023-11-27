009e681fdc4c4e4db0a15fead418bbf12463d94c
Mohed Nazari <astro@Moheds-MacBook-Pro.local>
2023-11-27 17:56:38 +0000

    updated about 2 contnet

--- components/homepages/home1/About2.js
+++ components/homepages/home1/About2.js
@@ -13,19 +13,12 @@ export default function About2() {
                                 <div className="heading2 no-margin-heading">
                                     <small data-aos="fade-right" data-aos-duration={600} className="heading-top"><img src="/assets/img/icons/hands.svg" alt="" />We Are Here For Your Tax
                                         Relief.</small>
-                                    <h2 data-aos="fade-right" data-aos-duration={800}>We Handle The Most Tedious Bookkeeping
-                                        Tasks of Your
-                                        Business Operations.</h2>
-                                    <p data-aos="fade-right" data-aos-duration={1000}>Experienced, knowledgeable professionals
-                                        in Property Tax
-                                        Consulting. Our team of Property Tax
-                                        Consultants is committed to saving you money by obtaining the lowest possible assessed
-                                        value for your
-                                        commercial or residential properties.
+                                    <h2 data-aos="fade-right" data-aos-duration={800}>Mastering Financial Complexities for Diverse Business Landscapes</h2>
+                                    <p data-aos="fade-right" data-aos-duration={1000}>Navigate your business's financial journey with our expert accounting services. Precision, compliance, and strategic insight come standard with our bookkeeping, tax, and consulting solutions. Join our portfolio of over 300 thriving businesses.
                                     </p>
                                     <div className="space50" />
                                     <div className="button-group" data-aos="fade-right" data-aos-duration={1200}>
-                                        <Link className="theme-btn-2" href="#">More About Us <span><i className="fa-solid fa-arrow-right" /></span></Link>
+                                        <Link className="theme-btn-2" href="#">Explore<span><i className="fa-solid fa-arrow-right" /></span></Link>
                                     </div>
                                 </div>
                             </div>
@@ -39,14 +32,14 @@ export default function About2() {
                                                 <img src="/assets/img/icons/counter21.png" alt="" />
                                             </div>
                                             <h2><CounterUp count={645}/>+</h2>
-                                            <p>Meetings So For</p>
+                                            <p>Successful Consultations</p>
                                         </div>
                                         <div className="single-counter" data-aos="zoom-out" data-aos-duration={800}>
                                             <div className="conter-icon">
                                                 <img src="/assets/img/icons/counter22.png" alt="" />
                                             </div>
-                                            <h2><CounterUp count={34}/>+</h2>
-                                            <p>Meetings So For</p>
+                                            <h2><CounterUp count={562}/>+</h2>
+                                            <p>Tax Strategies Implemented</p>
                                         </div>
                                     </div>
                                     <div className="col-md-6">
@@ -54,15 +47,15 @@ export default function About2() {
                                             <div className="conter-icon">
                                                 <img src="/assets/img/icons/counter23.png" alt="" />
                                             </div>
-                                            <h2><CounterUp count={562}/>+</h2>
-                                            <p>Meetings So For</p>
+                                            <h2><CounterUp count={34}/>+</h2>
+                                            <p>Specialized Trainings Conducted</p>
                                         </div>
                                         <div className="single-counter" data-aos="zoom-out" data-aos-duration={1200}>
                                             <div className="conter-icon">
                                                 <img src="/assets/img/icons/counter24.png" alt="" />
                                             </div>
-                                            <h2><CounterUp count={90}/>%</h2>
-                                            <p>Meetings So For</p>
+                                            <h2><CounterUp count={0}/>%</h2>
+                                            <p>Client Audit Issues</p>
                                         </div>
                                     </div>
                                 </div>
